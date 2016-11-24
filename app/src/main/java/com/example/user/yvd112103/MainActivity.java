98ddc1fb03c9832e185bf7067ce4c6886d074ee5
albert <alber@gmail.com>
2016-11-24 09:03:38 +0800

    use Gson to parse JSON file

--- app/src/main/java/com/example/user/yvd112103/MainActivity.java
+++ app/src/main/java/com/example/user/yvd112103/MainActivity.java
@@ -9,6 +9,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,24 +23,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        // 抓取新北市 opendata
         StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
-                        try {
-                            JSONArray array = new JSONArray(response);
-                            for (int i=0;i<array.length();i++)
-                            {
-                                JSONObject obj = array.getJSONObject(i);
-                                String str = obj.getString("district");
-                                Log.d("NET", str);
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    // -------- GSON 寫法 ---------
+                        Gson gson = new Gson();
+                        Animal loc[] = gson.fromJson(response, Animal[].class);
+                        for (Animal a : loc)
+                        {
+                            Log.d("NET", a.district);
                         }
+                    // -----------------------
 
+                    // ------- JSON 寫法 -------
+//                        try {
+//                            JSONArray array = new JSONArray(response);
+//                            for (int i=0;i<array.length();i++)
+//                            {
+//                                JSONObject obj = array.getJSONObject(i);
+//                                String str = obj.getString("district");
+//                                Log.d("NET", str);
+//                            }
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+                    // --------------------------
                     }
                 }, new Response.ErrorListener() {
             @Override
